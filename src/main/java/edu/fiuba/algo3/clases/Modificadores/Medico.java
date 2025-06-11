6aabaf44660b0279a69821780d8daac1c2e5d8fa
Sebastian <scolazo@fi.uba.ar>
2025-06-11 17:00:30 -0300

    Agrego version inicial del testMedico

--- src/main/java/edu/fiuba/algo3/clases/Modificadores/Medico.java
+++ src/main/java/edu/fiuba/algo3/clases/Modificadores/Medico.java
@@ -4,12 +4,25 @@ package edu.fiuba.algo3.clases.Modificadores;
 import edu.fiuba.algo3.clases.*;
 
 public class Medico extends Modificador {
-  public Medico() {
+  private Carta cartaARevivir;
+
+  public Medico(Carta cartaARevivir) {
+    this.cartaARevivir = cartaARevivir;
     this.esLegendaria = false;
   }
 
+
   @Override
   public void aplicarEfecto(Carta cartaContexto, Tablero tablero, Jugador Jugador) {
+    if(!jugador.getDescarte().contiene(cartaARevivir)){
+      throw new IllegalArgumentException("La carta a revivir no está en la pila de descarte");
+    }
 
+    jugador.getDescarte().quitarCarta(cartaARevivir);
+
+    if(!(cartaARevivir instanceof  Unidad)){
+      throw new UnsupportedOperationException("Solo se pueden revivir Unidades");
+    }
+    tablero.colocarUnidad(jugador, cartaARevivir, Posicion.A_DISTANCIA);
   }
 }
